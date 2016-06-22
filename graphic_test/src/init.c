526b7bcd9ad0726b6ae48b31acca9e761ec2ecba
Aurelie Orset <aurelie.orset@epitech.eu>
2016-06-22 15:39:03 +0200

    ajout de la barre info

--- graphic_test/src/init.c
+++ graphic_test/src/init.c
@@ -5,12 +5,12 @@
 ** Login   <orset_a@epitech.net>
 ** 
 ** Started on  Fri Jun 17 14:46:06 2016 Aurelie Orset
-** Last update Tue Jun 21 18:01:03 2016 Aurelie Orset
+** Last update Wed Jun 22 14:40:57 2016 Aurelie Orset
 */
 
 #include "graphic.h"
 
-SDL_Surface *init(char *title)
+SDL_Surface *init(char *title, int x, int y)
 {
   SDL_Surface *screen;
 
@@ -19,14 +19,12 @@ SDL_Surface *init(char *title)
       printf("Could not initialize SDL: %s\n", SDL_GetError());
       exit(1);
     }
-  screen = SDL_SetVideoMode(1080, 720, 0, SDL_HWPALETTE | SDL_DOUBLEBUF);
+  screen = SDL_SetVideoMode(x, y, 0, SDL_HWPALETTE | SDL_DOUBLEBUF);
   if (screen == NULL)
     {
       printf("Couldn't set screen mode to 640 x 480: %s\n", SDL_GetError());
       exit(1);
     }
   SDL_WM_SetCaption(title, NULL);
-  /* Hide the mouse cursor */
-  SDL_ShowCursor(SDL_DISABLE);
   return (screen);
 }
--- graphic_test/src/main.c
+++ graphic_test/src/main.c
@@ -5,23 +5,46 @@
 ** Login   <orset_a@epitech.net>
 ** 
 ** Started on  Fri Jun 17 14:48:10 2016 Aurelie Orset
-** Last update Mon Jun 20 16:55:20 2016 Aurelie Orset
+** Last update Wed Jun 22 15:23:03 2016 Aurelie Orset
 */
 
 #include "graphic.h"
-
+#include <SDL/SDL_ttf.h>
 int main(int ac, char **av)
 {
   unsigned int frameLimit = SDL_GetTicks() + 16;
   int go;
   SDL_Surface *screen;
+  int	mapx;
+  int	mapy;
+  /*  int	calc;
+  int	co;
+  double xx;
+  double yy;
+  int	x;
+  int	y;*/
 
-  screen = init("MOUILLETTE ZAPPY");
+  mapx = atoi(av[1]);
+  mapy = atoi(av[2]);
+  if(TTF_Init()==-1) {
+    printf("TTF_Init: %s\n", TTF_GetError());
+    exit(2);
+  }
+  /*calc = mapx;
+    if (calc > mapy)
+    calc = mapy;
+  co = 1000/calc;
+  co /= 96;
+  xx = 1080 - mapx * co;
+  x = xx;
+  yy = 1000 - mapy * co;
+  y = yy;*/
+  screen = init("MOUILLETTE ZAPPY", 1500, 1000);
   go = 1;
   while (go == 1)
     {
-      getInput();
-      draw(screen);
+      /*      getInput();*/
+      draw(screen, mapx, mapy);
       delay(frameLimit);
       frameLimit = SDL_GetTicks() + 16;
     }

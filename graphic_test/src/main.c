45cecb1a41e56d3bc2e1edfda26091f5f35cae60
orset_a <aurelie.orset@epitech.eu>
2016-06-23 12:54:08 +0200

    changement de prototype de certaines fonctions du gfx

--- graphic_test/src/main.c
+++ graphic_test/src/main.c
@@ -5,7 +5,7 @@
 ** Login   <orset_a@epitech.net>
 ** 
 ** Started on  Fri Jun 17 14:48:10 2016 Aurelie Orset
-** Last update Wed Jun 22 17:11:54 2016 Aurelie Orset
+** Last update Thu Jun 23 12:53:25 2016 Aurelie Orset
 */
 
 #include "graphic.h"
@@ -17,28 +17,13 @@ int main(int ac, char **av)
   SDL_Surface *screen;
   int	mapx;
   int	mapy;
-  /*  int	calc;
-  int	co;
-  double xx;
-  double yy;
-  int	x;
-  int	y;*/
 
   mapx = atoi(av[1]);
   mapy = atoi(av[2]);
-  if(TTF_Init()==-1) {
+  if (TTF_Init()==-1) {
     printf("TTF_Init: %s\n", TTF_GetError());
     exit(2);
   }
-  /*calc = mapx;
-    if (calc > mapy)
-    calc = mapy;
-  co = 1000/calc;
-  co /= 96;
-  xx = 1080 - mapx * co;
-  x = xx;
-  yy = 1000 - mapy * co;
-  y = yy;*/
   screen = init("MOUILLETTE ZAPPY", 1500, 1000);
   go = 1;
   while (go == 1)

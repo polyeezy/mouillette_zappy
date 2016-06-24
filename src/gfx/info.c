120cb8c03652ba18d3a718c20031341a7fcbfb9e
orset_a <aurelie.orset@epitech.eu>
2016-06-24 18:46:37 +0200

    fix du segfault + add invetaire et lvl player

--- src/gfx/info.c
+++ src/gfx/info.c
@@ -5,7 +5,7 @@
 ** Login   <orset_a@epitech.net>
 ** 
 ** Started on  Wed Jun 22 14:18:08 2016 Aurelie Orset
-** Last update Fri Jun 24 16:01:10 2016 Aurelie Orset
+** Last update Fri Jun 24 18:44:05 2016 Aurelie Orset
 */
 
 #include "graphic.h"
@@ -28,13 +28,52 @@ void	drawTexte(t_info *i, int x, int y, char *str)
   SDL_FreeSurface(texte);
 }
 
-void	drawInventaire(t_graph *g, int id, t_client_socket client)
+
+int	getID(t_client_socket client, t_graph *g)
+{
+  char	*str;
+  int	x;
+  int	y;
+  int	players;
+  int	mapx;
+  int	mapy;
+  char	**tab;
+  int	i;
+  int	l;
+
+  l = 2;
+  i = 0;
+  players = 0;
+  str = malloc(sizeof(char) * 4096);
+  str = send_and_get_gfx(&client, "lpy");
+  str = "lpy 4\n4 1 6 6 0\n3 2 7 7 0\n32 3 5 5 0\n56 4 4 4 1\n";
+  tab = my_str_to_wordtab(str, " \n");
+  if (strcmp(tab[0], "lpy") == 0)
+    players = atoi(tab[1]);
+  SDL_GetMouseState(&x, &y);
+  x = convertX(x, g);
+  y = convertX(y, g);
+  while (i < players)
+    {
+      mapx = atoi(tab[l + 2]);
+      mapy = atoi(tab[l + 3]);
+      if (mapx == x && mapy == y)
+	return (atoi(tab[l]));
+      i++;
+      l+=5;
+    }
+  return (-1);
+}
+
+void	drawInventaire(t_graph *g, t_client_socket client)
 {
   char *str;
   char	**tab;
+  int	id;
 
   t_info	*i;
 
+  id = getID(client, g);
   i = init_info(g);
   i = resize_info(i, 0.2);
   if ((i->police = TTF_OpenFont("gfx/font.ttf", 35)) == NULL)
@@ -42,9 +81,11 @@ void	drawInventaire(t_graph *g, int id, t_client_socket client)
   str = malloc(sizeof(char) * 255);
   sprintf(str, "pin %d", id);
   str = send_and_get_gfx(&client, str);
+  str = "pin 36 56 56 0 1 2 3 4 5 6\n";
   tab = my_str_to_wordtab(str, " \n");
   printf("INVENTAIRE : %s\n", str);
-  if (strcmp(tab[0], "sbp") != 0)
+  printf("NOT HERE PLS\n");
+  if (strcmp(tab[0], "sbp") != 0 && id != -1)
     {
       drawImage(i->l, 1050, 750, g->screen);
       drawTexte(i, 1100, 750, tab[5]);
@@ -61,32 +102,41 @@ void	drawInventaire(t_graph *g, int id, t_client_socket client)
       drawImage(i->n, 1200, 870, g->screen);
       drawTexte(i, 1250, 870, tab[4]);
     }
-  free(str);
+  /*  free(str);*/
+  printf("YOLO\n");
 }
 
-void	drawPlayer(t_info *i, t_graph *g, int id, int lvl)
+void	drawPlayer(t_info *i, t_graph *g, t_client_socket client)
 {
   SDL_Surface *texte;
   SDL_Surface *texte2;
   char str[18];
+  int	id;
+  int lvl;
 
-  sprintf(str, " %d", id);
-  texte = TTF_RenderText_Solid(i->police, "PLAYER", i->color);
-  texte2 = TTF_RenderText_Solid(i->police, str, i->color);
-  drawImage(texte, 1015, 550, g->screen);
-  drawImage(texte2, 1200, 550, g->screen);
-  SDL_FreeSurface(texte);
-  SDL_FreeSurface(texte2);
-  sprintf(str, " %d", lvl);
-  texte = TTF_RenderText_Solid(i->police, "LEVEL", i->color);
-  texte2 = TTF_RenderText_Solid(i->police, str, i->color);
-  drawImage(texte, 1015, 620, g->screen);
-  drawImage(texte2, 1200, 620, g->screen);
-  SDL_FreeSurface(texte);
-  SDL_FreeSurface(texte2);
-  texte = TTF_RenderText_Solid(i->police, "Inventaire", i->color);
-  drawImage(texte, 1100, 690, g->screen);
-  SDL_FreeSurface(texte);
+  lvl = 0;
+  id = getID(client, g);
+  printf("ID : %d\n", id);
+  if (id != -1)
+    {
+      sprintf(str, " %d", id);
+      texte = TTF_RenderText_Solid(i->police, "PLAYER", i->color);
+      texte2 = TTF_RenderText_Solid(i->police, str, i->color);
+      drawImage(texte, 1015, 550, g->screen);
+      drawImage(texte2, 1200, 550, g->screen);
+      SDL_FreeSurface(texte);
+      SDL_FreeSurface(texte2);
+      sprintf(str, " %d", lvl);
+      texte = TTF_RenderText_Solid(i->police, "LEVEL", i->color);
+      texte2 = TTF_RenderText_Solid(i->police, str, i->color);
+      drawImage(texte, 1015, 620, g->screen);
+      drawImage(texte2, 1200, 620, g->screen);
+      SDL_FreeSurface(texte);
+      SDL_FreeSurface(texte2);
+      texte = TTF_RenderText_Solid(i->police, "Inventaire", i->color);
+      drawImage(texte, 1100, 690, g->screen);
+      SDL_FreeSurface(texte);
+    }
 }
 
 void	drawFood(t_info *i, int x, int y, t_client_socket client)
@@ -160,17 +210,13 @@ void	drawInfo(int x, int y, t_graph *g, t_client_socket client)
 {
   SDL_Event event;
   t_info *i;
-  int	id;
-  int	lvl;
 
-  id = 2;
   i = init_info(g);
   i = resize_info(i, 0.5);
   while (SDL_PollEvent(&event))
     {
       if (event.type == SDL_MOUSEBUTTONDOWN)
 	{
-	  lvl = getLvl(client, id);
 	  SDL_GetMouseState(&x, &y);
 	  x = convertX(x, g);
 	  y = convertX(y, g);
@@ -179,8 +225,8 @@ void	drawInfo(int x, int y, t_graph *g, t_client_socket client)
 	      drawCoord(i, g, x, y);
 	      drawFood(i, x, y, client);
 	    }
-	  drawPlayer(i, g, id, lvl);
-	  drawInventaire(g, id, client);
+	  drawPlayer(i, g, client);
+	  drawInventaire(g, client);
 	}
       else if (event.type == SDL_QUIT)
 	{
---
+++ src/gfx/players.c
@@ -0,0 +1,64 @@
+
+/*
+** players.c for zappy in /home/orset_a/tek2/zappy/mouillette_zappy/src/gfx
+** 
+** Made by Aurelie Orset
+** Login   <orset_a@epitech.net>
+** 
+** Started on  Fri Jun 24 16:00:41 2016 Aurelie Orset
+** Last update Fri Jun 24 18:36:21 2016 Aurelie Orset
+*/
+
+#include "graphic.h"
+
+char	**getToParse(t_client_socket client)
+{
+  char **tab;
+  char *str;
+
+  str = malloc(sizeof(char) * 255);
+  str = send_and_get_gfx(&client, "lpy");
+  str = "lpy 4\n4 1 6 6 0\n3 2 7 7 0\n32 3 5 5 0\n56 4 4 4 1\n";
+  tab = my_str_to_wordtab(str, " \n");
+  return (tab);
+}
+
+void	drawOrient(t_graph *g, int x, char **tab)
+{
+  int mapx;
+  int mapy;
+
+  mapx = atoi(tab[x + 2]);
+  mapy = atoi(tab[x + 3]);
+  if (atoi(tab[x + 4]) == 1)
+    drawImage(g->incant, mapx * g->ts, mapy * g->ts, g->screen);
+  if (atoi(tab[x + 1]) == 2)
+    drawImage(g->nord, mapx * g->ts, mapy * g->ts, g->screen);
+  else if (atoi(tab[x + 1]) == 2)
+    drawImage(g->est, mapx * g->ts, mapy * g->ts, g->screen);
+  else if (atoi(tab[x + 1]) == 3)
+    drawImage(g->sud, mapx * g->ts, mapy * g->ts, g->screen);
+  else
+    drawImage(g->ouest, mapx * g->ts, mapy * g->ts, g->screen);
+}
+
+void	drawPlayers(t_graph *g, t_client_socket client)
+{
+  char **tab;
+  int	players;
+  int	i;
+  int	x;
+
+  i = 0;
+  x = 2;
+  players = 0;
+  tab = getToParse(client);
+  if (strcmp(tab[0], "lpy") == 0)
+    players = atoi(tab[1]);
+  while (i < players)
+    {
+      drawOrient(g, x, tab);
+      x+= 5;
+      i++;
+    }
+}

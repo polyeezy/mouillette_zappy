1b53981a3a52f05b5253cbf8408274cec7b16e3b
polizz_v <valerian.polizzi@epitech.eu>
2016-06-25 12:05:16 +0200

    oubli de client_get_food.c

---
+++ src/client/client_get_food.c
@@ -0,0 +1,47 @@
+/*
+** client_get_food.c for  in /home/polizz_v/rendu/cPP/mouillette_zappy/src/client
+** 
+** Made by Valerian Polizzi
+** Login   <polizz_v@epitech.net>
+** 
+** Started on  Sat Jun 25 10:41:33 2016 Valerian Polizzi
+** Last update Sat Jun 25 11:22:19 2016 Valerian Polizzi
+*/
+
+#include <client.h>
+
+void            client_get_food(t_ai *cli)
+{
+  int           cell;
+  char          **parsing;
+  int           rotations;
+
+  cell = -1;
+  parsing = NULL;
+  parsing = parse_voir(cli);
+
+  while ((cell = look_for_object(parsing, "nourriture")) != 0)
+    {
+      rotations = 0;
+      while (cell == -1 && rotations < 4)
+	{
+          ai_gauche(cli);
+          parsing = parse_voir(cli);
+          cell = look_for_object(parsing, "nourriture");
+          rotations += 1;
+	}
+      if (cell == -1)
+	ai_avance(cli);
+      else if (cell == 0)
+	ai_prend(cli, "nourriture");
+      else
+        {
+	  parse_inventaire(cli);
+	  ai_count_move(cli, cell);
+          free_tab(parsing);
+          parsing = parse_voir(cli);
+	}
+    }
+  ai_prend(cli, "nourriture");
+}
+

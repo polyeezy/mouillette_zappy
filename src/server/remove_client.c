945c835756c8daf92e7908dc236ad84f9d0e7c6a
Alexis Miele <alexis.miele@epitech.eu>
2016-06-25 17:54:26 +0200

    Fix remove from map

--- src/server/remove_client.c
+++ src/server/remove_client.c
@@ -5,32 +5,30 @@
 ** Login   <weinha_l@epitech.eu>
 **
 ** Started on  Thu Jun 23 17:21:17 2016 Loïc Weinhard
-** Last update Sat Jun 25 16:30:15 2016 Loïc Weinhard
+** Last update Sat Jun 25 17:53:32 2016 Alexis Miele
 */
 
 #include "xfct.h"
 #include "server.h"
 
-static t_case	**remove_from_map(t_case **map, t_client *player)
+static void	remove_from_map(t_client **client, t_client *player)
 {
   t_client	*tmp;
 
-  tmp = map[player->y][player->x].players;
+  tmp = *client;
   while (tmp)
     {
       if (tmp->fd == player->fd)
 	{
 	  tmp->prev != NULL ? tmp->prev->next = tmp->next : 0;
 	  tmp->next != NULL ? tmp->next->prev = tmp->prev : 0;
-          tmp->prev == NULL && tmp->next == NULL ? tmp = NULL : 0;
-	  while (tmp && tmp->prev)
-	    tmp = tmp->prev;
-	  map[player->y][player->x].players = tmp;
-	  return (map);
+          if (tmp->prev == NULL && tmp->next == NULL)
+	    *client = NULL;
+	  return;
 	}
       tmp = tmp->next;
     }
-  return (NULL);
+  return;
 }
 
 static void	remove_from_team(t_team **teams, t_client *player)
@@ -69,7 +67,7 @@ void		remove_client(t_server *server, t_client *player, int ret)
 	{
 	  if (tmp->fd == player->fd)
 	    {
-	      server->map = remove_from_map(server->map, tmp);
+	      remove_from_map(&(server->map[tmp->y][tmp->x].players), tmp);
 	      remove_from_team(&(server->teams), tmp);
 	      xclose(player->fd);
 	      xfree(player);

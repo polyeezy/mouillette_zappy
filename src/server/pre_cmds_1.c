2c38c11f979e746632533bd629d566dfc82b9b84
Alexis Miele <alexis.miele@epitech.eu>
2016-06-26 21:14:03 +0200

    Check len pre cmd

--- src/server/pre_cmds_1.c
+++ src/server/pre_cmds_1.c
@@ -5,38 +5,84 @@
 ** Login   <miele_a@epitech.net>
 **
 ** Started on  Sun Jun 26 13:17:21 2016 Alexis Miele
-** Last update Sun Jun 26 18:03:55 2016 Aurelie Orset
+** Last update Sun Jun 26 21:12:45 2016 Alexis Miele
 */
 
 #include "cmds.h"
 #include "pile.h"
+#include "xfct.h"
 
 void	pre_avance(t_server *server, t_client *player,
 		   char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void	pre_droite(t_server *server, t_client *player,
 		   char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void	pre_gauche(t_server *server, t_client *player,
 		   char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void	pre_voir(t_server *server, t_client *player,
 		 char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void	pre_inventaire(t_server *server, t_client *player,
 		       char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
--- src/server/pre_cmds_2.c
+++ src/server/pre_cmds_2.c
@@ -5,25 +5,53 @@
 ** Login   <miele_a@epitech.net>
 **
 ** Started on  Sun Jun 26 13:28:48 2016 Alexis Miele
-** Last update Sun Jun 26 15:42:56 2016 Alexis Miele
+** Last update Sun Jun 26 21:12:56 2016 Alexis Miele
 */
 
 #include "cmds.h"
 #include "pile.h"
+#include "xfct.h"
 
 void    pre_prend(t_server *server, t_client *player, char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 2)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void    pre_pose(t_server *server, t_client *player, char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 2)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
 void    pre_broadcast(t_server *server, t_client *player,
 		      char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) < 2)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
 
@@ -35,5 +63,14 @@ void    pre_mange(t_server *server, t_client *player, char *buffer, t_cmd *cmd)
 void    pre_connect_nbr(t_server *server, t_client *player,
 			char *buffer, t_cmd *cmd)
 {
+  char	**tab;
+
+  tab = my_str_to_wordtab(buffer, " \n");
+  if (lentab(tab) != 1)
+    {
+      xwrite(player->fd, "ko\n");
+      free_tab(tab);
+      return;
+    }
   add_pile(server, player, strdup(buffer), cmd);
 }
